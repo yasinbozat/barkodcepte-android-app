c0e77aa4c40e1b875af9198d81c4686aa78d8db2
Yasin Bozat <yasinbozatr@gmail.com>
2020-06-28 18:51:55 +0300

    Added login activity

--- BarkodCepte/app/src/main/java/com/bc/barkodcepte/login.java
+++ BarkodCepte/app/src/main/java/com/bc/barkodcepte/login.java
@@ -5,16 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         //FULL SCREEN CODES ------------------------------------------------------------------------
         getSupportActionBar().hide();
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                                                         WindowManager.LayoutParams.FLAG_FULLSCREEN);
         //------------------------------------------------------------------------------------------
+
     }
 }
