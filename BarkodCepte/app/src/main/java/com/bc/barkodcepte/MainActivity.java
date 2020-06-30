d8530207e9d3ba3343a83f4fcc3adfd5bb4657ad
Yasin Bozat <yasinbozatr@gmail.com>
2020-06-30 15:56:59 +0300

    Ürün ekle ve görüntüle

--- BarkodCepte/app/src/main/java/com/bc/barkodcepte/MainActivity.java
+++ BarkodCepte/app/src/main/java/com/bc/barkodcepte/MainActivity.java
@@ -2,13 +2,44 @@ package com.bc.barkodcepte;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_ekle, btn_goruntule;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_ekle = findViewById(R.id.btn_ekle);
+        btn_goruntule = findViewById(R.id.btn_Goruntule);
+
+        btn_ekle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(MainActivity.this,AddProductActivity.class);
+                startActivity(i);
+
+            }
+        });
+        btn_goruntule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(MainActivity.this,ViewProductActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+
+
+
+
     }
 }
