a08e156e13e4fb15226c6956425c101d39877c6b
Yasin Bozat <yasinbozatr@gmail.com>
2020-06-30 19:20:31 +0300

    Add product activty barcode

--- BarkodCepte/app/src/main/java/com/bc/barkodcepte/AddProductActivity.java
+++ BarkodCepte/app/src/main/java/com/bc/barkodcepte/AddProductActivity.java
@@ -1,19 +1,43 @@
 package com.bc.barkodcepte;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
 
 public class AddProductActivity extends AppCompatActivity {
 
     private Button btn_AddProduct;
     private EditText edt_ProductBardcode, edt_ProductName, edt_ProductPrice;
 
+    //--Kameradan Barkod Okuma--//
+    private SurfaceView surfaceView;
+    private BarcodeDetector barcodeDetector;
+    private CameraSource cameraSource;
+    private static final int REQUEST_CAMERA_PERMISSION = 201;
+    private ToneGenerator toneGen1;
+    private String barcodeData;
+    //--Kameradan Barkod Okuma--//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +52,11 @@ public class AddProductActivity extends AppCompatActivity {
         edt_ProductName = findViewById(R.id.edt_ProductName);
         edt_ProductPrice = findViewById(R.id.edt_ProductPrice);
 
+        //--Kameradan Barkod Okuma--//
+        toneGen1 = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
+        surfaceView = findViewById(R.id.surface_addProduct);
+
+        //--Kameradan Barkod Okuma--//
         Database db = new Database(AddProductActivity.this);
 
         btn_AddProduct.setOnClickListener(new View.OnClickListener() {
@@ -40,14 +69,113 @@ public class AddProductActivity extends AppCompatActivity {
                     String productPrice = edt_ProductPrice.getText().toString();
 
                     db.InsertData(productBarcode, productName, productPrice);
-                    Log.d("DEBUG","eklendi");
+                    Log.d("DEBUG", "Eklendi");
                 }catch (Exception e){
-                    Log.d("DEBUG", "aaaaaaaaaa"+String.valueOf(e));
+                    Log.d("DEBUG", String.valueOf(e));
                 }
 
             }
         });
 
 
+    }
+
+    private void initialiseDetectorsAndSources() {
+
+        //Toast.makeText(getApplicationContext(), "Barcode scanner started", Toast.LENGTH_SHORT).show();
+
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.ALL_FORMATS)
+                .build();
+
+        cameraSource = new CameraSource.Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(1920, 1080)
+                .setAutoFocusEnabled(true) //you should add this feature
+                .build();
+
+
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+
+                try {
+                    if (ActivityCompat.checkSelfPermission(AddProductActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                        cameraSource.start(surfaceView.getHolder());
+                    } else {
+                        ActivityCompat.requestPermissions(AddProductActivity.this, new
+                                String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+                // Toast.makeText(getApplicationContext(), "To prevent memory leaks barcode scanner has been stopped", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+                if (barcodes.size() != 0) {
+
+                    edt_ProductBardcode.post(new Runnable() {
+
+                        @Override
+
+                        public void run() {
+
+
+                            if (barcodes.valueAt(0).email != null) {
+
+                                barcodeData = barcodes.valueAt(0).email.address;
+                                edt_ProductBardcode.setText(barcodeData);
+                                toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 150);
+                            } else {
+                                barcodeData = barcodes.valueAt(0).displayValue;
+                                edt_ProductBardcode.setText(barcodeData);
+                                toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 150);
+
+                            }
+                        }
+                    });
+
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        cameraSource.release();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initialiseDetectorsAndSources();
     }
 }
