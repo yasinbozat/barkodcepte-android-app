0a2508fc550fe116602707115c3135b6cd29fd66
fatihsuzen <53154457+fatihsuzen@users.noreply.github.com>
2020-07-12 15:36:51 +0300

    addstock

--- BarkodCepte/app/src/main/java/com/bc/barkodcepte/AddStockActivity.java
+++ BarkodCepte/app/src/main/java/com/bc/barkodcepte/AddStockActivity.java
@@ -20,55 +20,70 @@ public class AddStockActivity extends AppCompatActivity {
     ArrayAdapter<String > adapter;
     EditText stok;
     Button kaydet;
+    String urunadi=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_stock);
         kaydet =  findViewById(R.id.btn_Kaydet);
         stok  =    findViewById(R.id.edit_stok);
+        lv = findViewById(R.id.stok_list);
         final ArrayList<String> list = new ArrayList<String>();
         adapter= new ArrayAdapter<String>(AddStockActivity.this,android.R.layout.simple_list_item_1,list);
+
+
+        Database d = new Database(AddStockActivity.this);
+        SQLiteDatabase db = d.getReadableDatabase();
+        String esittir = "=";
+        String sorgu = "SELECT urunAdi,urunStok FROM urunler ";
+        final Cursor c = db.rawQuery(sorgu, null);
+        while (c.moveToNext()) {
+            adapter.add(c.getString(0)+" "+c.getString(1));
+
+            adapter.notifyDataSetChanged();
+
+        }
         lv.setAdapter(adapter);
+        c.close();
+
         kaydet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              /*  try {
+                try {
                     Database d = new Database(AddStockActivity.this);
                     SQLiteDatabase db = d.getReadableDatabase();
                     String esittir = "=";
-                    String sorgu = "SELECT urunAdi,urunFiyat FROM urunler WHERE barkod" + esittir + stok.getText().toString();
-                    Cursor c = db.rawQuery(sorgu, null);
-                    if (c != null) {
-                        c.moveToFirst();
 
-                        urun_isim.setText(c.getString(0));
-                        urun_fiyat.setText(c.getString(1));
-                        adapter.add(c.getString(0));
-                        urun_toplam += c.getFloat(1);
-                        float a =  c.getFloat(1);
-                        adapter_toplam.add(String.valueOf(a));
-                        toplam.setText(urun_toplam+"");
-                    }
-
-                    c.close();
+                    String sorgu1 = "UPDATE urunler SET urunStok"+esittir+stok.getText()+" WHERE urunAdi" + esittir +"'"+ urunadi+"'";
+                    db.execSQL(sorgu1);
+                    Toast.makeText(getApplicationContext(),"STOK GÜNCELLENDİ",Toast.LENGTH_SHORT).show();
+                    
 
                 }
                 catch (Exception e)
                 {e.printStackTrace();
-                    Toast.makeText(getApplicationContext(),"KAYITSIZ ÜRÜN",Toast.LENGTH_SHORT).show();
-                } */
+                    Toast.makeText(getApplicationContext(),"BİR SORUN OLUŞTU",Toast.LENGTH_SHORT).show();
+                }
+                adapter.notifyDataSetChanged();
             }
         });
 
-        lv.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-                stok.setText(lv.getSelectedItem().toString());
-            }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
+                urunadi = adapter.getItem(position).substring(0,adapter.getItem(position).indexOf(' '));
+                Database d = new Database(AddStockActivity.this);
+                SQLiteDatabase db = d.getReadableDatabase();
+                String esittir = "=";
+                String sorgu = "SELECT urunStok FROM urunler WHERE urunAdi='"+urunadi+"'";
+                Cursor c1 = db.rawQuery(sorgu, null);
+                if (c1!=null) {
+                    c1.moveToFirst();
+                    stok.setText(c1.getString(0));
+                    adapter.notifyDataSetChanged();
+                }
 
             }
         });
